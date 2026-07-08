4e002dbc2aea83f78e4c46756106ab6629b156b1
Akshay Sutar <akshayrsutar358@gmail.com>
2026-07-08 11:12:55 +0530

    String methods

---
+++ string-methods.py
@@ -0,0 +1,20 @@
+print("String methods in python :\n")
+
+name="akshay sutar "
+job="and i wants to become a good devops engineer"
+"""
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.replace("Akshay","Sutar"))
+print(name)
+print(name[0])
+print(name[1:5])
+print(name.capitalize())
+print(name.title())
+print(name.startswith("Ak"))
+print(name.endswith("y"))
+print(name.index("a"))
+"""
+combine=name+""+job
+print(combine)
